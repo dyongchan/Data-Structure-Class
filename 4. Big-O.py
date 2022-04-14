918a3f59bd783c49fb43fd7faafaaddb63b7e5a7
조용찬 <101387953+dyongchan@users.noreply.github.com>
2022-04-15 00:42:28 +0900

    Add files via upload

--- 4. Big-O.py
+++ 4. Big-O.py
@@ -1,56 +1,52 @@
-from pydoc import doc
 
+알고리즘의 시간복잡도 (time complexity) : Big-O 표기법 # 최고차항으로 간단하게 수행시간을 표기한다.
 
-알고리즘의 시간복잡도 (time complexity)
-# 알고리즘 수행시간의 정의
+알고리즘 예시 3가지 이용
 
-기본연산을 얼마나 수행하는가
-A = [, , , , , ,] / n : input size
-1. 모든 입력에 대하여 기본연산 횟수를 더한 후 평균 # 무한한 입력일 시 현실적으로 불가능
-2. 가장 안좋은 입력(worst case input)에 대한 기본연산 횟수를 측정 : worstcase time complexity
-# 어떤 입력에 대해서도 worstcase time complexity 보다 수행시간이 크지 않다.
+1. Algorithm1 : T1(n) = 2n - 1 # 1차함수
 
-알고리즘 수행시간 = 최악의 입력에 대한 기본연산 횟수로 정의
+2. Algorithm2 : T2(n) = 4n - 1 # 1차함수
 
+3. Algorithm3 : T3(n) = 3/2n^2 - 3/2bn + 1 # 2차함수
+
+- Algorithm2가 Algorithm1 보다 2배 느리다.
+
+- Algorithm3는 n < 5/3 면 Algorithm2 보다 빠르다.
+- Algorithm3는 n > 5/3 면 항상 Algorithm2 보다 느리다.
+- Algorithm3는 모든 n에 대하여 Algorithm1보다 느리다.
+# 그래프를 통하여 확인가능
+
+T1(n) , T2(n) : n에 대하여 선형적으로 증가한다. (1차함수 - 직선의 특징) # 최고차항 n
+T3(n) : n에 대하여 제곱으로 증가한다. # 최고차항 n^2 
+= 무한대로 보낼때 T3(n)이 훨씬 빠르게 증가한다.
+= 함숫값을 결정하는 중요한 것 : 증가율 - 최고차항이 중요
+
+T1(n) = 2n - 1 / T1(n) = O(n)
+T2(n) = 4n - 1 / T2(m) = O(n)
+T3(n) = 3/2n^2 - 3/2bn + 1 /  T3(n) = O(n^2)\
+# 증가율을 기준으로 하므로 계수는 신경쓰지 않고 차수만 적는다.
+# O(n)은 O(n^2)의 집합에 포함된다.
+
+''' 표기법
+1. 최고차항만 남긴다
+2. 최고차항 계수(상수)는 생략
+Big-O(최고차항)
 '''
-예시1) 
-algorithm arrayMax(A,n):
-    currentMax = A[0] # 대입의 기본연산 1회
-    for i = 1 to n-1 do
-        if currentMax < A[i]: # 비교의 기본연산 1회
-            current = A[i] # 비교문장이 참 일때만 수행
-    return currentMax
-비교문장이 계속 참이되어 기본수행을 계속 수행 하는 것이 최악의 경우
-# A가 오름차순의 array 구조 일 때 가장 최악의 수행시간이 됨.
 
-A[0]의 선언 : 기본연산 1회
-for 문 : 1 부터 n-1번까지 수행
-if 문 : 비교문의 항상 참으로 기본연산 계속 시행 
-for 문 * if 문의 기본연산 : 2n-2 / + 1 (A[0])의 명시 = 2n-1
-T(n) : 2n-1 # n = 6 / T(6) = 12-1 = 11
-'''
-'''
+예시1)
+def increment_one(a): # a라는 매개변수에 값을 증가시켜주는 함수
+    return a + 1 # T(n) = 1 / 최고차항 : n^0 - O(1)
+
 예시2)
-algorithm sum1(A,n): # n에 비례하여 수행시간 상승 (1차식)
-    sum = 0 # 대입 : 기본연산 1회
-    for i = 0 to n-1 doc # n번 돈다
-        if A[i]%2 == 0: # 나머지, 비교 2회 기본연산 / 짝수이면
-            sum += A[i] # sum = sum + A{i}
-    return sum
+def numberof_bits(n): # n라는 값을 받아서 2진수로 몇비트가 필요한지 보는 함수
+    count = 0
+    while n > 0: # while 총 4번 기본연산 및 log2^n번 돈다
+        n = n//2 # 정수 나눗셈 : 몫만표현
+        count += 1 
+    return count
 
-A : 모든값이 짝수 : worstcase 입력
-T(n) = 4n + 1
-# n에 비례하여 수행시간 상승 (1차식)
-'''
-'''
-예시3)
-algorithm sum2(A,n):
-    sum = 0 # 기본연산 1회
-    for i = 0 to n-1 do # n번 반복
-        for j = i to n-1 do # i = 0,1,2...n-1 / j = 1+2+3+...+n = n(n+1)/2 
-            sum +=A[i] * A[j] # 기본연산 3회 [n(n+1)/2 만큼 반복]
-    return sum
-
-T(n) = n(n+1)/2 * 3 + 1 = 3/2 n^2 + 3/2 n + 1
-# n의 제곱에 비례하여 수행시간 상승 (2차식)
-'''
+n = 8 이 주어질 때,
+n = 8 - 4 - 2 - 1 - 0 / count = 3 # 0이되면 빠져나오게 됨
+n = n/2 - n/2^2 - ... [ n/2^count = 1 ] / n = 2^count / log2^n = count
+따라서 T(n) = c*log2^n + 1 = O(log2^n)
+# log2^n은 n 보다 수행시간이 빠르다 - 그래프로 확인가능
